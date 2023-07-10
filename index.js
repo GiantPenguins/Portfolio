97d61d5c63561538554a33da4b3a9af636a86268
GiantPenguins <80788588+GiantPenguins@users.noreply.github.com>
2023-07-10 13:47:42 +0200

    Add files via upload
    Navigation added to the left column, not styled.
    Java script updated to be easily read.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-var cursorShadow = document.querySelector(".cursor-shadow");
+// Cursor
+
+let cursorShadow = document.querySelector(".cursor-shadow");
 document.addEventListener("mousemove", function (e) {
-  var xPos = e.clientX - cursorShadow.clientWidth / 2;
-  var yPos = e.clientY - cursorShadow.clientHeight / 2;
+  let xPos = e.clientX - cursorShadow.clientWidth / 2;
+  let yPos = e.clientY - cursorShadow.clientHeight / 2;
   cursorShadow.style.left = xPos + "px";
   cursorShadow.style.top = yPos + "px";
+});
+
+// Navigation
+
+window.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".navigation-container nav ul li");
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      navLinks.forEach(function (link) {
+        link.classList.remove("active");
+      });
+
+      this.classList.add("active");
+    });
+  });
 });
